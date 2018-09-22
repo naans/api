bd77760ebfbe6fcf6421f1e7db701898344b398c
Amine Ben hammou <webneat@gmail.com>
2018-09-22 10:50:23 +0200

    wip

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const R    = require('ramda')
-    , api  = require('wajez-api')
+    , wz   = require('wajez-api')
     , config = require('config')
     , express = require('express')
+    , mongoose = require('mongoose')
+    , bodyParser = require('body-parser')
     , Category = require('./resources/Category')
     , Extra = require('./resources/Extra')
     , Info = require('./resources/Info')
@@ -12,55 +14,72 @@ const R    = require('ramda')
     , users = require('./routes/users')
     , {ids, id} = require('./utils')
 
-const app = api({
-    database: `mongodb://localhost/${config.get('database')}`,
-    auth: {
-        secret: config.get('secret'),
-        Model: User,
-        fields: ['name', 'password'],
-        route: '/auth',
-        ignore: req => req.method == 'GET' || req.path == '/auth'
-    },
-    errorHandler: (err, req, res, next) => {
-        let error = {}, status = 500
-        switch (err.name) {
-            case 'ValidationError':
-                status = 400
-                for(let name in err.errors)
-                    error[name] = err.errors[name].message
-                error._error = 'Erreur de validation'
-            break
-            case 'MongoError':
-                status = 400
-                error._error = 'Erreur de validation'
-            break
-    
-            case 'UnauthorizedError':
-                status = 401
-                error._error = 'Acces Refuse'
-            break
-            case 'WrongCredentialsError':
-                status = 401
-                error._error = 'Nom ou mot de passe incorrect'
-                error.name = true
-                error.password = true
-            break
-            default:
-                status = 500
-                error._error = 'Unknkown Error'
-        }
-        console.error(err)
-        res.status(status).json(error)
-    }
-})
+mongoose.Promise = global.Promise
+mongoose.connect(config.get('database'), {useNewUrlParser: true})
 
+const app = express()
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}))
+app.use(bodyParser.json({limit: '50mb'}))
 app.use((req, res, next) => {
     console.log(req)
     next()
 })
-
 app.use('/photos', express.static(`${config.get('root')}/${config.get('pictures_path')}`))
 
+const resources = [Category, Extra, Info, Meal, Slide, User]
+const relations = [
+    wz.oneMany('Category', 'meals', 'Meal', 'category'),
+    wz.oneMany('Meal', 'extras', 'Extra', null)
+]
+const auth = wz.auth({secret: config.get('secret')}, User)
+app.use(wz.router([
+  wz.login(config.get('secret'), User, ['name', 'password'], {uri: '/auth'})
+]))
+
+app.use(wz.api(resources, relations, {
+    _all: {
+        add: {actions: [wz.beforeQuery(auth)]},
+        edit: {actions: [wz.beforeQuery(auth)]},
+        destroy: {actions: [wz.beforeQuery(auth)]}
+    }
+}))
+
+app.use(pictures)
+app.use(users)
+
+app.use((err, req, res, next) => {
+    let error = {}, status = 500
+    switch (err.name) {
+        case 'ValidationError':
+            status = 400
+            for(let name in err.errors)
+                error[name] = err.errors[name].message
+            error._error = 'Erreur de validation'
+        break
+        case 'MongoError':
+            status = 400
+            error._error = 'Erreur de validation'
+        break
+
+        case 'UnauthorizedError':
+            status = 401
+            error._error = 'Acces Refuse'
+        break
+        case 'WrongCredentialsError':
+            status = 401
+            error._error = 'Nom ou mot de passe incorrect'
+            error.name = true
+            error.password = true
+        break
+        default:
+            status = 500
+            error._error = 'Unknkown Error'
+    }
+    console.error(err)
+    res.status(status).json(error)
+})
+
+/*
 app.resource(Category, {
     json: Category.json,
     children: [{
@@ -96,13 +115,10 @@ app.resource(Extra, {json: Extra.json})
 app.resource(Info, {json: Info.json})
 app.resource(Slide, {json: Slide.json})
 
-app.use(pictures)
-app.use(users)
 
 app.handleErrors()
+*/
 
 app.listen(3001)
 
 module.exports = app
-
-require('./setup')()
